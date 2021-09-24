bd75190e5a6b666320a57da8537c3ed382cb702b
Даниял Магомедов <Magomedov.D@edu.rea.ru>
2021-09-24 21:25:39 +0300

    Add 2.2

---
+++ 2.2/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BigInteger
+{
+    public enum Sign
+    {
+        Minus = -1,
+        Plus = 1
+    }
+
+    public class BigNumber
+    {
+        private readonly List<byte> digits = new List<byte>();
+
+        public BigNumber(List<byte> bytes)
+        {
+            digits = bytes.ToList();
+            RemoveNulls();
+        }
+
+        public BigNumber(Sign sign, List<byte> bytes)
+        {
+            Sign = sign;
+            digits = bytes;
+            RemoveNulls();
+        }
+
+        public BigNumber(string s)
+        {
+            if (s.StartsWith("-"))
+            {
+                Sign = Sign.Minus;
+                s = s.Substring(1);
+            }
+
+            foreach (var c in s.Reverse())
+            {
+                digits.Add(Convert.ToByte(c.ToString()));
+            }
+
+            RemoveNulls();
+        }
+
+        public BigNumber(uint x) => digits.AddRange(GetBytes(x));
+
+        public BigNumber(int x)
+        {
+            if (x < 0)
+            {
+                Sign = Sign.Minus;
+            }
+
+            digits.AddRange(GetBytes((uint)Math.Abs(x)));
+        }
+
+        private List<byte> GetBytes(uint num)
+        {
+            var bytes = new List<byte>();
+            do
+            {
+                bytes.Add((byte)(num % 10));
+                num /= 10;
+            } while (num > 0);
+
+            return bytes;
+        }
+
+        private void RemoveNulls()
+        {
+            for (var i = digits.Count - 1; i > 0; i--)
+            {
+                if (digits[i] == 0)
+                {
+                    digits.RemoveAt(i);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public static BigNumber Exp(byte val, int exp)
+        {
+            var bigInt = Zero;
+            bigInt.SetByte(exp, val);
+            bigInt.RemoveNulls();
+            return bigInt;
+        }
+
+        public static BigNumber Zero => new BigNumber(0);
+        public static BigNumber One => new BigNumber(1);
+
+        // Длина числа
+        public int Size => digits.Count;
+
+        // Знак числа
+        public Sign Sign { get; private set; } = Sign.Plus;
+
+        // Получение цифры по индексу
+        public byte GetByte(int i) => i < Size ? digits[i] : (byte)0;
+
+        // Установка цифры по индексу
+        public void SetByte(int i, byte b)
+        {
+            while (digits.Count <= i)
+            {
+                digits.Add(0);
+            }
+
+            digits[i] = b;
+        }
+
+        // Преобразование длинного числа в строку
+        public override string ToString()
+        {
+            if (this == Zero) return "0";
+            var s = new StringBuilder(Sign == Sign.Plus ? "" : "-");
+
+            for (int i = digits.Count - 1; i >= 0; i--)
+            {
+                s.Append(Convert.ToString(digits[i]));
+            }
+
+            return s.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
